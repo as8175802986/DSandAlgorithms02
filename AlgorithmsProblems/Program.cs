ecd50d7513e76deb08d4c57b73d453db0350dd2a
“Arunesh <as8175802986@gmail.com>
2021-11-28 22:34:52 +0530

    [Arunesh]Added: code for Algorithms problem

---
+++ AlgorithmsProblems/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter your Choice Number to Execute the Program Press- 1-Binary Search,2-Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                   
+                    case 1:
+                        string txtpath = File.ReadAllText(@"E:\DSandAlgorithms02\DSandAlgorithms02\AlgorithmsProblems\Utility\WordFile.txt");
+                        List<string> words = new List<string>(txtpath.Split());
+                        words.Sort();
+                        BinarySearch.BinarySearchWord(words);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+
+        }
+    }
+}

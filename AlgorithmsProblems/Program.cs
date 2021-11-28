9c3bbd5511fb2d3fb832663df0708b6b4562706e
“Arunesh <as8175802986@gmail.com>
2021-11-28 22:54:42 +0530

    [Arunesh]Added: code for Insertion Sort

--- AlgorithmsProblems/Program.cs
+++ AlgorithmsProblems/Program.cs
@@ -14,7 +14,7 @@ namespace AlgorithmsProblems
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter your Choice to Execute the Program Press- 1-Binary Search,2-Bubble sort,3-Exit");
+                Console.WriteLine("Enter your Choice to Execute the Program Press- 1-Binary Search,2-Bubble sort,3-InsertionShort,4-Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -31,6 +31,12 @@ namespace AlgorithmsProblems
                         BubbleSort.PrintArray(arr1);
                         break;
                     case 3:
+                        InsertionShort insertionSort = new InsertionShort();
+                        int[] arr = { 10, 2, 14, 3, 1, 5, 9 };
+                        insertionSort.Sort(arr);
+                        insertionSort.PrintArray(arr);
+                        break;
+                    case 4:
                         flag = false;
                         break;
                 }
